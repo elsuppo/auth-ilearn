a6f227c2f38082786ccb2a4312caa596f17e6680
elsuppo <andrew.suponev@gmail.com>
2023-01-19 00:50:56 +0300

    feat: implement log out

--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -1,46 +1,41 @@
-import React, {useState, useEffect} from 'react';
-import { DataGrid } from '@mui/x-data-grid'; 
+import { DataGrid } from '@mui/x-data-grid';
+import { useNavigate } from 'react-router-dom';
 
 const cols = [
-  {field: 'name', headerName: 'name', width: 200},
-  {field: 'email', headerName: 'email', width: 200},
-  {field: 'phone', headerName: 'phone', width: 200},
+  {field: 'id', headerName: 'id', width: 50},
+  {field: 'name', headerName: 'name', width: 100},
+  {field: 'email', headerName: 'email', width: 100},
+  {field: 'dateReg', headerName: 'dateReg', width: 150},
+  {field: 'dateLastLog', headerName: 'dateLastLog', width: 150},
+  {field: 'status', headerName: 'status', width: 50},
 ];
 
+const rows = [
+  {id: 1, name: 'blank', email: 'blank', dateReg: 'blank data', dateLastLog: 'blank data', status: 'block'}
+]
+
+
 const Users = () => {
+  const navigate = useNavigate();
 
-  const [users, setUsers] = useState([]);
-
-  const fetchData = async () => {
-    try {
-      const res = await fetch('https://jsonplaceholder.typicode.com/users');
-      const data = await res.json();
-      setUsers(data);
-      console.log(data);
-    } catch (e) {
-      console.log(e);
-    }
+  const logOut = () => {
+    navigate('/login');
   }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const rowData = users?.map(user => {
-    return {
-      name: user?.name,
-      email: user?.email,
-      phone: user?.phone,
-      id: user?.id
-    }
-  })
-
   return (
-    <div style={{width: "100%"}}>
+    <div className="container-md d-flex justify-content-center flex-column" style={{width: "100%"}}>
+      <div className="d-flex justify-content-between align-items-center p-3">
+        <p className="m-0">Table of users</p>
+        <button 
+          className="btn btn-secondary m-0"
+          onClick={logOut}
+        >Log out</button>
+      </div>
+
       <DataGrid
         checkboxSelection
         autoHeight
-        rows={rowData}
+        rows={rows}
         columns={cols}
       />
     </div>
