4a65f3aa20be2e50a72889564589e4415f0f4df0
Andrew <andrew.suponev@gmail.com>
2023-01-21 13:29:54 +0300

    fix: change method authorization (server part)

--- server/controllers/AuthControllers.js
+++ server/controllers/AuthControllers.js
@@ -9,24 +9,30 @@ const createToken = (id) => {
   });
 };
 
+const updateLastLoginDate = (id) => {
+  UserModel.findOneAndUpdate({_id: id}, {dateLastLogin: new Date()}, (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(`login user: ${data}`);
+    }
+  })
+}
+
 const handleErrors = (error) => {
   let errors = { name: '', email: '', password: '' };
-
   if (error.message === 'incorrect email') {
     errors.email = 'that email is not register';
     return errors;
   }
-
   if (error.message === 'incorrect password') {
     errors.password = 'that password is incorrect';
     return errors;
   }
-
   if (error.code === 11000) {
     errors.email = 'user with this email already exists';
     return errors;
   }
-
   if (error.message.includes('Users validation failed')) {
     Object.values(error.errors).forEach(({ properties }) => {
       errors[properties.path] = properties.message;
@@ -40,8 +46,10 @@ module.exports.register = async (req, res) => {
     const { name, email, password } = req.body;
     const user = await UserModel.create({ name, email, password });
     const token = createToken(user._id);
-    res.cookie('jwt', token, {withCredentials: true, httpOnly: false, maxAge: maxAge * 1000});
-    res.status(201).json({ user: user._id, created: true });
+
+    // res.cookie('jwt', token, {withCredentials: true, httpOnly: false, maxAge: maxAge * 1000});
+
+    res.status(201).json({ user: user._id, created: true, token });
   } catch (error) {
     console.log(error);
     const errors = handleErrors(error);
@@ -54,17 +62,9 @@ module.exports.login = async (req, res) => {
     const { email, password } = req.body;
     const user = await UserModel.login(email, password);
     const token = createToken(user._id);
-
-    res.cookie('jwt', token, {httpOnly: false, maxAge: maxAge * 1000});
-    res.status(200).json({ user: user._id, status: true })
-
-    UserModel.findOneAndUpdate({_id: user._id}, {dateLastLogin: new Date()}, (error, data) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log(`login user: ${data}`);
-      }
-    })
+    // res.cookie('jwt', token, {httpOnly: false, maxAge: maxAge * 1000});
+    updateLastLoginDate(user._id);
+    res.status(200).json({ user: user._id, status: true, token})
   } catch (error) {
     console.log(error);
     const errors = handleErrors(error);
--- server/middlewares/AuthMiddlewares.js
+++ server/middlewares/AuthMiddlewares.js
@@ -3,22 +3,18 @@ const jwt = require('jsonwebtoken');
 
 module.exports.checkUser = async (req, res, next) => {
   try {
-    const token = req.url.slice(9);
+    const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
     if (token) {
       jwt.verify(token, process.env.JWT_ACCESS_SECRET, async (error, decodedToken) => {
         if (error) {
           res.status(403).json({ message: 'no access' });
         } else {
           const user = await User.findById(decodedToken.id);
-          if (user) {
-            if (user.statusUser === 'blocked') {
-              res.status(403).json({ message: 'no access' });
-            } else {
-              console.log('the user is verified');
-              next();
-            }
+          if (!user || user.statusUser === 'blocked') {
+            return res.status(403).json({ message: 'user not found or user blocked' });
           } else {
-            res.status(403).json({ message: 'no access' });
+            console.log('user is verified');
+            next();
           }
         }
       });
